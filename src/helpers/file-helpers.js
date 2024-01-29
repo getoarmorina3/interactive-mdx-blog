7e8ee28a87d7c0da40486581b3dc719f2a57409d
getoarmorina3 <getoaarm1@gmail.com>
2024-01-29 22:09:41 +0100

    Update file-helpers.js

--- src/helpers/file-helpers.js
+++ src/helpers/file-helpers.js
@@ -2,15 +2,15 @@ import fs from 'fs/promises';
 import path from 'path';
 import matter from 'gray-matter';
 
+import React from 'react';
+
 export async function getBlogPostList() {
   const fileNames = await readDirectory('/content');
 
   const blogPosts = [];
 
   for (let fileName of fileNames) {
-    const rawContent = await readFile(
-      `/content/${fileName}`
-    );
+    const rawContent = await readFile(`/content/${fileName}`);
 
     const { data: frontmatter } = matter(rawContent);
 
@@ -20,31 +20,21 @@ export async function getBlogPostList() {
     });
   }
 
-  return blogPosts.sort((p1, p2) =>
-    p1.publishedOn < p2.publishedOn ? 1 : -1
-  );
+  return blogPosts.sort((p1, p2) => (p1.publishedOn < p2.publishedOn ? 1 : -1));
 }
 
-export async function loadBlogPost(slug) {
-  const rawContent = await readFile(
-    `/content/${slug}.mdx`
-  );
+export const loadBlogPost = React.cache(async function loadBlogPost(slug) {
+  const rawContent = await readFile(`/content/${slug}.mdx`);
 
-  const { data: frontmatter, content } =
-    matter(rawContent);
+  const { data: frontmatter, content } = matter(rawContent);
 
   return { frontmatter, content };
-}
+});
 
 function readFile(localPath) {
-  return fs.readFile(
-    path.join(process.cwd(), localPath),
-    'utf8'
-  );
+  return fs.readFile(path.join(process.cwd(), localPath), 'utf8');
 }
 
 function readDirectory(localPath) {
-  return fs.readdir(
-    path.join(process.cwd(), localPath)
-  );
+  return fs.readdir(path.join(process.cwd(), localPath));
 }
