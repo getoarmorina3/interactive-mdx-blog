f1bb1744785200562ac2141ca3af6de323e772d0
getoarmorina3 <getoaarm1@gmail.com>
2024-02-01 16:28:22 +0100

    add motion config preference support

---
+++ src/components/RespectMotionPreferences/RespectMotionPreferences.js
@@ -0,0 +1,10 @@
+'use client';
+
+import React from 'react';
+import { MotionConfig } from 'framer-motion';
+
+function RespectMotionPreferences({ children }) {
+  return <MotionConfig reducedMotion="user">{children}</MotionConfig>;
+}
+
+export default RespectMotionPreferences;
