61d130910030d3608aba22fa15f8e48e3027c484
getoarmorina3 <getoaarm1@gmail.com>
2024-02-03 00:20:53 +0100

    lazy load CircularColorsDemo

--- src/components/CircularColorsDemo/index.js
+++ src/components/CircularColorsDemo/index.js
@@ -1,2 +1,5 @@
-export * from './CircularColorsDemo';
-export { default } from './CircularColorsDemo';
+import dynamic from 'next/dynamic';
+
+const CircularColorsDemo = dynamic(() => import('./CircularColorsDemo'));
+
+export default CircularColorsDemo;
